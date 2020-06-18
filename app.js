125a8ffc20bfb9d9c35b4d484b8df0cb17bb7462
linlinliu <linlinliu03@gmail.com>
2020-06-18 11:30:03 -0700

    Add methods on notes.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Initial setup and some test examples
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes");
+const notes = require("./notes");
 
 // customize yargs version
 yargs.version("1.1.0")
@@ -23,8 +23,7 @@ yargs.command({
         }
     },
     handler: function(argv){
-        console.log("Title:"  + argv.title)
-        console.log("Body:"  + argv.body)
+        notes.addNotes(argv.title, argv.body)
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,6 +1,26 @@
+const fs = require("fs")
 
 const getNotes = () => {
     return "Your notes..."
 }
 
-module.exports = getNotes;
+const addNotes = (title, body) => {
+   const notes = loadNotes();
+   console.log(notes)
+}
+
+const loadNotes = () => {
+    try{
+         const dataBuffer = fs.readFileSync("notes.json");
+         const dataJson = dataBuffer.toString();
+         return JSON.parse(dataJson)
+
+    }catch(e){
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNotes
+};
